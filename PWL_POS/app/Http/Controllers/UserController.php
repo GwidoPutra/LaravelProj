bc3e1704f08645746db15412874a0435c5d60b04
gwidopw <gwidoputra@gmail.com>
2025-03-09 09:48:18 +0700

    praktikum 2.2

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -11,10 +11,16 @@ class UserController extends Controller
 {
     public function index()
     {
+        // $user = UserModel::find(1);  --> Praktikum 2.1
+        // return view('user', ['data' => $user]);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function() {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+        //     abort(404);
+        // }); --> Praktikum 2.1
+
+        // $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
 
         return view('user', ['data' => $user]);
 
