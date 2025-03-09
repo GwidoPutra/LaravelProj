cf7dc5f3a685b6c1c43e7fac108629bdcae3d5ca
gwidopw <gwidoputra@gmail.com>
2025-03-09 10:09:41 +0700

    praktikum 2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -24,8 +24,21 @@ class UserController extends Controller
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
         // --> Praktikum 2.2 <--
 
-        $user = UserModel::where('level_id', 2)->count();
-        dd($user);
+
+        // --> Praktikum 2.3 <--
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+        // --> Praktikum 2.3 <--
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data' => $user]);
 
 
