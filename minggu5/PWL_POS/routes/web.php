bfe40a890120f379df1afb1faf8cfb0e0b0aa3e0
gwidopw <gwidoputra@gmail.com>
2025-03-15 13:25:18 +0700

    tugas praktikum 2, 3 & laporan

--- minggu5/PWL_POS/routes/web.php
+++ minggu5/PWL_POS/routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\KategoriController;
-
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,11 +20,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/kategori/create', [KategoriController::class, 'create']);
-Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/level',[LevelController::class,'index']);
+Route::get('/kategori',[KategoriController::class,'index']);
+Route::get('/user',[UserController::class,'index']);
+
+Route::get('/user/tambah',[UserController::class,'tambah']);
+Route::get('/user/tambah_simpan',[UserController::class,'tambah_simpan']);
+
+Route::get('/user/ubah/{id}',[UserController::class,'ubah']);
+Route::put('/user/ubah_simpan/{id}',[UserController::class,'ubah_simpan']);
+
+Route::get('/user/hapus/{id}',[UserController::class,'hapus']);
+Route::get('kategori',[KategoriController::class,'index'])->name('kategori.index');
 Route::get('kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+Route::post('/kategori', [KategoriController::class, 'store']);
+
+Route::get('kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::post('kategori/edit', [KategoriController::class, 'update']);
+Route::get('kategori/hapus/{id}', [KategoriController::class, 'delete']);
