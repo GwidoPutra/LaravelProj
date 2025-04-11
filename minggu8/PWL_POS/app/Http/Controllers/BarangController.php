e0b796d68199fc229d3f1c39f76b35d419d0e949
gwidopw <gwidoputra@gmail.com>
2025-04-11 20:47:54 +0700

    praktikum 1 jobsheet 8

---
+++ minggu8/PWL_POS/app/Http/Controllers/BarangController.php
@@ -0,0 +1,359 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BarangModel;
+use Illuminate\Http\Request;
+use App\Models\KategoriModel;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
+
+class BarangController extends Controller
+{
+    // Menampilkan halaman daftar barang
+    public function index()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Daftar Barang',
+            'list' => ['Home', 'Barang']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar barang dalam sistem'
+        ];
+
+        $activeMenu = 'barang';
+        $kategori = KategoriModel::all();
+
+        return view('barang.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'kategori' => $kategori]);
+    }
+
+    // Ambil data barang dalam bentuk JSON untuk DataTables
+    public function list(Request $request)
+    {
+        $barang = BarangModel::select('barang_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'kategori_id')->with('kategori');
+
+        // if ($request->kategori_id) {
+        //     $barang->where('kategori_id', $request->kategori_id);
+        // }
+
+        $kategori_id = $request->input('filter_kategori');
+        if(!empty($kategori_id)){
+            $barang->where('kategori_id', $kategori_id);
+        }
+
+        return DataTables::of($barang)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($barang) {
+                // $btn = '<a href="' . url('/barang/' . $barang->barang_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                // $btn .= '<a href="' . url('/barang/' . $barang->barang_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                // $btn .= '<form class="d-inline-block" method="POST" action="'
+                //     . url('/barang/' . $barang->barang_id) . '">' . csrf_field()
+                //     . method_field('DELETE') . '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                $btn = '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/barang/' . $barang->barang_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button>';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+
+    // Menampilkan halaman tambah barang
+    public function create()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Tambah Barang',
+            'list' => ['Home', 'Barang', 'Tambah']
+        ];
+
+        $page = (object) [
+            'title' => 'Tambah barang baru'
+        ];
+
+        $activeMenu = 'barang';
+        $kategori = KategoriModel::all();
+
+        return view('barang.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'kategori' => $kategori]);
+    }
+
+    // Menyimpan data barang baru
+    public function store(Request $request)
+    {
+        $request->validate([
+            'barang_kode' => 'required|string|min:2|unique:m_barang,barang_kode',
+            'kategori_id' => 'required|integer',
+            'barang_nama' => 'required|string|max:100',
+            'harga_beli' => 'required|integer',
+            'harga_jual' => 'required|integer'
+        ]);
+
+        BarangModel::create($request->all());
+
+        return redirect('/barang')->with('success', 'Data Barang berhasil disimpan');
+    }
+
+    // Menampilkan detail barang
+    public function show($id)
+    {
+        $barang = BarangModel::findOrFail($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Detail Barang',
+            'list' => ['Home', 'Barang', 'Detail']
+        ];
+
+        $page = (object) [
+            'title' => 'Detail barang'
+        ];
+
+        $activeMenu = 'barang';
+
+        return view('barang.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'barang' => $barang, 'activeMenu' => $activeMenu]);
+    }
+
+    // Menampilkan halaman edit barang
+    public function edit($id)
+    {
+        $barang = BarangModel::findOrFail($id);
+        $kategori = KategoriModel::all();
+
+        $breadcrumb = (object) [
+            'title' => 'Edit Barang',
+            'list' => ['Home', 'Barang', 'Edit']
+        ];
+
+        $page = (object) [
+            'title' => 'Edit barang'
+        ];
+
+        $activeMenu = 'barang';
+
+        return view('barang.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'barang' => $barang, 'activeMenu' => $activeMenu, 'kategori' => $kategori]);
+    }
+
+    // Menyimpan perubahan data barang
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'barang_kode' => 'required|string|min:2|unique:m_barang,barang_kode,' . $id . ',barang_id',
+            'kategori_id' => 'required|integer',
+            'barang_nama' => 'required|string|max:100',
+            'harga_beli' => 'required|integer',
+            'harga_jual' => 'required|integer'
+        ]);
+
+        BarangModel::findOrFail($id)->update($request->all());
+
+        return redirect('/barang')->with('success', 'Data Barang berhasil diubah');
+    }
+
+    // Menghapus data barang
+    public function destroy($id)
+    {
+        $check = BarangModel::find($id);
+        if (!$check) {
+            return redirect('/barang')->with('error', 'Data Barang tidak ditemukan');
+        }
+        try {
+            BarangModel::destroy($id);
+            return redirect('/barang')->with('success', 'Data Barang berhasil dihapus');
+
+        } catch (\Illuminate\Database\QueryException $e) {
+            return redirect('/barang')->with('error', 'Data Barang gagal dihapus karena masih terdapat data lain yang terkait');
+        }
+    }
+
+    // Menampilkan modal tambah barang
+    public function create_ajax()
+    {
+        $kategori = KategoriModel::select('kategori_id', 'kategori_nama')->get();
+
+        return view('barang.create_ajax')->with('kategori', $kategori);
+    }
+
+    // Menyimpan data barang baru dengan ajax
+    public function store_ajax(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'kategori_id' => ['required', 'integer', 'exists:m_kategori,kategori_id'],
+                'barang_kode' => ['required', 'min:3', 'max:20', 'unique:m_barang,barang_kode'],
+                'barang_nama' => ['required', 'string', 'max:100'],
+                'harga_beli' => ['required', 'numeric'],
+                'harga_jual' => ['required', 'numeric'],
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+
+            BarangModel::create($request->all());
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data barang berhasil disimpan'
+            ]);
+        }
+        return redirect('/');
+    }
+
+    // Menampilkan detail barang dengan ajax
+    public function show_ajax(string $id)
+    {
+        $barang = BarangModel::with('kategori')->find($id);
+
+        return view('barang.show_ajax', ['barang' => $barang]);
+    }
+
+    // Menampilkan modal edit barang
+    public function edit_ajax(string $id)
+    {
+        $barang = BarangModel::find($id);
+        $kategori = KategoriModel::select('kategori_id', 'kategori_nama')->get();
+
+        return view('barang.edit_ajax', ['barang' => $barang, 'kategori' => $kategori]);
+    }
+
+    // Menyimpan perubahan data barang dengan ajax
+    public function update_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'kategori_id' => ['required', 'integer', 'exists:m_kategori,kategori_id'],
+                'barang_kode' => ['required', 'min:3', 'max:20', 'unique:m_barang,barang_kode, '. $id .',barang_id'],
+                'barang_nama' => ['required', 'string', 'max:100'],
+                'harga_beli' => ['required', 'numeric'],
+                'harga_jual' => ['required', 'numeric'],
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi gagal.',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+
+            try {
+                BarangModel::find($id)->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diupdate'
+                ]);
+            } catch (\Illuminate\Database\QueryException $e) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi gagal.',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
+    // Menampilkan modal konfirmasi
+    public function confirm_ajax(string $id)
+    {
+        $barang = BarangModel::find($id);
+
+        return view('barang.confirm_ajax', ['barang' => $barang]);
+    }
+
+    // Menghapus data barang
+    public function delete_ajax(Request $request, $id)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $barang = BarangModel::find($id);
+            if ($barang) {
+                try {
+                    $barang->delete();
+                    return response()->json([
+                        'status' => true,
+                        'message' => 'Data berhasil dihapus'
+                    ]);
+                } catch (\Illuminate\Database\QueryException $e) {
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'Data tidak bisa dihapus'
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
+    // import data
+    public function import()
+    {
+        return view('barang.import');
+    }
+
+    public function import_ajax(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                // validasi file harus xls atau xlsx, max 1MB
+                'file_barang' => ['required', 'mimes:xlsx', 'max:1024']
+            ];
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+            $file = $request->file('file_barang'); // ambil file dari request
+            $reader = IOFactory::createReader('Xlsx'); // load reader file excel
+            $reader->setReadDataOnly(true); // hanya membaca data
+            $spreadsheet = $reader->load($file->getRealPath()); // load file excel
+            $sheet = $spreadsheet->getActiveSheet(); // ambil sheet yang aktif
+            $data = $sheet->toArray(null, false, true, true); // ambil data excel
+            $insert = [];
+            if (count($data) > 1) { // jika data lebih dari 1 baris
+                foreach ($data as $baris => $value) {
+                    if ($baris > 1) { // baris ke 1 adalah header, maka lewati
+                        $insert[] = [
+                            'kategori_id' => $value['A'],
+                            'barang_kode' => $value['B'],
+                            'barang_nama' => $value['C'],
+                            'harga_beli' => $value['D'],
+                            'harga_jual' => $value['E'],
+                            'created_at' => now(),
+                        ];
+                    }
+                }
+                if (count($insert) > 0) {
+                    // insert data ke database, jika data sudah ada, maka diabaikan
+                    BarangModel::insertOrIgnore($insert);
+                }
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diimport'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Tidak ada data yang diimport'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+}
---
+++ minggu8/PWL_POS/routes/web.php
@@ -0,0 +1,137 @@
+<?php
+
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AuthorizeUser;
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::pattern('id', '[0-9]+');
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postLogin']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+
+Route::get('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'postRegister']);
+
+Route::middleware(['auth'])->group(function () {
+   Route::get('/', [WelcomeController::class, 'index']);
+
+   // artinya semua route di dalam group ini harus punya role ADM
+   Route::middleware(['authorize:ADM'])->group(function () {
+      Route::get('/user/', [UserController::class, 'index']);
+      Route::post('/user/list', [UserController::class, 'list']);
+      Route::get('/user/create', [UserController::class, 'create']);
+      Route::post('/user/', [UserController::class, 'store']);
+      Route::get('/user/create_ajax', [UserController::class, 'create_ajax']);
+      Route::post('/user/ajax', [UserController::class, 'store_ajax']);
+      Route::get('/user/{id}', [UserController::class, 'show']);
+      Route::get('/user/{id}/edit', [UserController::class, 'edit']);
+      Route::put('/user/{id}', [UserController::class, 'update']);
+      Route::get('/user/{id}/show_ajax', [UserController::class, 'show_ajax']);
+      Route::get('/user/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+      Route::put('/user/{id}/update_ajax', [UserController::class, 'update_ajax']);
+      Route::get('/user/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+      Route::delete('/user/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
+      Route::delete('/user/{id}', [UserController::class, 'destroy']);
+   });
+
+
+
+   // artinya semua route di dalam group ini harus punya role ADM (Administrator)
+   Route::middleware(['authorize:ADM'])->group(function () {
+      Route::get('/level/', [LevelController::class, 'index']);
+      Route::post('/level/list', [LevelController::class, 'list']);
+      Route::get('/level/create', [LevelController::class, 'create']);
+      Route::post('/level/', [LevelController::class, 'store']);
+      Route::get('/level/create_ajax', [LevelController::class, 'create_ajax']); // Menampilkan halaman form tambah user Ajax
+      Route::post('/level/ajax', [LevelController::class, 'store_ajax']); // Menyimpan data user baru Ajax
+      Route::get('/level/{id}', [LevelController::class, 'show']);
+      Route::get('/level/{id}/show_ajax', [LevelController::class, 'show_ajax']);
+      Route::get('/level/{id}/edit', [LevelController::class, 'edit']);
+      Route::put('/level/{id}', [LevelController::class, 'update']);
+      Route::get('/level/{id}/edit_ajax', [LevelController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax
+      Route::put('/level/{id}/update_ajax', [LevelController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
+      Route::get('/level/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
+      Route::delete('/level/{id}/delete_ajax', [LevelController::class, 'delete_ajax']); // Untuk hapus data user Ajax
+      Route::delete('/level/{id}', [LevelController::class, 'destroy']);
+   });
+
+   // artinya semua route di dalam group ini harus punya roke ADM dan MNG
+   Route::middleware(['authorize:ADM,MNG'])->group(function () {
+      Route::get('/kategori', [KategoriController::class, 'index']);
+      Route::post('/kategori/list', [KategoriController::class, 'list']);
+      Route::get('/kategori/create', [KategoriController::class, 'create']);
+      Route::post('/kategori/', [KategoriController::class, 'store']);
+      Route::get('/kategori/create_ajax', [KategoriController::class, 'create_ajax']);
+      Route::post('/kategori/ajax', [KategoriController::class, 'store_ajax']);
+      Route::get('/kategori/{id}', [KategoriController::class, 'show']);
+      Route::get('/kategori/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
+      Route::get('/kategori/{id}/edit', [KategoriController::class, 'edit']);
+      Route::put('/kategori/{id}', [KategoriController::class, 'update']);
+      Route::get('/kategori/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
+      Route::put('/kategori/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
+      Route::get('/kategori/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
+      Route::delete('/kategori/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
+      Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']);
+   });
+
+   // artinya semua route di dalam group ini harus punya role ADM (Administrator) dan MNG (Manager)
+   Route::middleware(['authorize:ADM,MNG'])->group(function () {
+      Route::get('/barang', [BarangController::class, 'index']);
+      Route::post('/barang/list', [BarangController::class, 'list']);
+      Route::get('/barang/create_ajax', [BarangController::class, 'create_ajax']); // ajax form create
+      Route::post('/barang_ajax', [BarangController::class, 'store_ajax']);        // ajax store
+      Route::get('/barang/{id}/show_ajax', [BarangController::class, 'show_ajax']); // ajax show
+      Route::get('/barang/{id}/edit_ajax', [BarangController::class, 'edit_ajax']); // ajax form edit
+      Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']); // ajax update
+      Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); // ajax form confirm
+      Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // ajax delete
+      Route::get('/barang/import', [BarangController::class, 'import']); // ajax form upload excel
+      Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+   });
+
+   // artinya semua route di dalam group ini harus punya role ADM dan MNG
+   Route::middleware(['authorize:ADM,MNG'])->group(function () {
+      Route::get('/supplier', [SupplierController::class, 'index']);
+      Route::post('/supplier/list', [SupplierController::class, 'list']);
+      Route::get('/supplier/create', [SupplierController::class, 'create']);
+      Route::post('/supplier/', [SupplierController::class, 'store']);
+      Route::get('/supplier/create_ajax', [SupplierController::class, 'create_ajax']); 
+      Route::post('/supplier/ajax', [SupplierController::class, 'store_ajax']);
+      Route::get('/supplier/{id}', [SupplierController::class, 'show']);
+      Route::get('/supplier/{id}/show_ajax', [SupplierController::class, 'show_ajax']);
+      Route::get('/supplier/{id}/edit', [SupplierController::class, 'edit']);
+      Route::put('/supplier/{id}', [SupplierController::class, 'update']);
+      Route::get('/supplier/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']); 
+      Route::put('/supplier/{id}/update_ajax', [SupplierController::class, 'update_ajax']); 
+      Route::get('/supplier/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']); 
+      Route::delete('/supplier/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']); 
+      Route::delete('/supplier/{id}', [SupplierController::class, 'destroy']);
+   });
+
+   Route::post('/logout', function () {
+      Auth::logout();
+      request()->session()->invalidate();
+      request()->session()->regenerateToken();
+      return redirect('/login');
+   })->name('logout');
+
+});
