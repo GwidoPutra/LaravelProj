11a3b7a0642ff6267390216c7f50767497652a96
gwidopw <gwidoputra@gmail.com>
2025-04-12 14:33:42 +0700

    praktikum 3 jobsheet 8

--- minggu8/PWL_POS/routes/web.php
+++ minggu8/PWL_POS/routes/web.php
@@ -107,6 +107,7 @@ Route::middleware(['auth'])->group(function () {
       Route::get('/barang/import', [BarangController::class, 'import']); // ajax form upload excel
       Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
       Route::get('barang/export_excel', [BarangController::class, 'export_excel']); //export excel)
+      Route::get('barang/export_pdf', [BarangController::class, 'export_pdf']); //export pdf)
    });
 
    // artinya semua route di dalam group ini harus punya role ADM dan MNG
