300e3c68383a17c27e65a39d089a3a414ccfcd04
gwidopw <gwidoputra@gmail.com>
2025-04-13 14:10:24 +0700

    merubah tampilan dashboard

--- PWL_POS/PWL_POS/resources/views/welcome.blade.php
+++ PWL_POS/PWL_POS/resources/views/welcome.blade.php
@@ -1,15 +1,26 @@
 @extends('layouts.template')
 
 @section('content')
+    <div class="container-fluid">
+        </div>
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Halo, apa kabar!!!</h3>
-        <div class="card-tools"></div>
+        <div class="row">
+            @foreach ($infoBoxes as $box)
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-{{ $box['color'] }}">
+                        <div class="inner">
+                            <h3>{{ $box['count'] }}</h3>
+                            <p>{{ $box['title'] }}</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas {{ $box['icon'] }}"></i>
+                        </div>
+                        <a href="{{ $box['url'] }}" class="small-box-footer">
+                            Lihat Detail <i class="fas fa-arrow-circle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-    <div class="card-body">
-        Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
-    </div>
-</div>
-
 @endsection
