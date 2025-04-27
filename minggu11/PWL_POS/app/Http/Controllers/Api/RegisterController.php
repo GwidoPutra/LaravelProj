5269fc95006c0fed608c322fdcbc9030b811f040
gwidopw <gwidoputra@gmail.com>
2025-04-27 13:10:07 +0700

    praktikum 1 jobsheet 11

--- minggu11/PWL_POS/app/Http/Controllers/Api/RegisterController.php
+++ minggu11/PWL_POS/app/Http/Controllers/Api/RegisterController.php
@@ -16,7 +16,8 @@ class RegisterController extends Controller
             'username' => 'required',
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
-            'level_id' => 'required'
+            'level_id' => 'required',
+            'profile_picture' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         //if validation fails
@@ -30,6 +31,7 @@ class RegisterController extends Controller
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
+            'profile_picture' => $request->profile_picture->hashName(),
         ]);
 
         //return response JSON user is created
